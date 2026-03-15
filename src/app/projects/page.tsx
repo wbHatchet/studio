f11b1bb2c1c8ad973d310326c415a7ea41a86d35
Jimmie Rabon Jr <rabonjrjimmie@gmail.com>
2026-03-15 17:20:40 +0000

    Try fixing this error: `Runtime ReferenceError: cn is not defined. Error

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -8,6 +8,7 @@ import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/com
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
 import { 
   PlusCircle, 
   Layers, 
