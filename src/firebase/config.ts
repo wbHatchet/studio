a7ae5c2d3bf1866f2880fed397ba0bc5c2246ed0
Jimmie Rabon Jr <rabonjrjimmie@gmail.com>
2026-03-15 01:25:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-204278647-b1ebe",
+  "appId": "1:616334125696:web:af26a3df958844f9481465",
+  "apiKey": "AIzaSyD9idSnxGOPyvipKyjjHHiQAiykudfp71Q",
+  "authDomain": "studio-204278647-b1ebe.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "616334125696"
+};
